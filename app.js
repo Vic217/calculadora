a830c96f0554bf3ccda892be38fcf956b5e585c2
VicGar21 <victorgarp15@hotmail.com>
2023-08-06 17:44:16 -0600

    Agrega var y funciones para agregar valores teclas

--- app.js
+++ app.js
@@ -68,3 +68,57 @@ function operate(primerNum, operador, segundoNum){
             return console.error("Operación no valida");
     }
 }
+
+// Variables de control
+const pantalla = document.getElementById("operacion");
+const botonCero = document.getElementById("cero");
+const botonUno = document.getElementById("uno");
+const botonDos = document.getElementById("dos");
+const botonTres = document.getElementById("tres");
+const botonCuatro = document.getElementById("cuatro");
+const botonCinco = document.getElementById("cinco");
+const botonSeis = document.getElementById("seis");
+const botonSiete = document.getElementById("siete");
+const botonOcho = document.getElementById("ocho");
+const botonNueve = document.getElementById("nueve");
+
+// Agregar el valor del botón en la pantalla
+botonCero.addEventListener("click", (e) => {
+    pantalla.innerText += 0;
+});
+
+botonUno.addEventListener("click", (e) => {
+    pantalla.innerText += 1;
+});
+
+botonDos.addEventListener("click", (e) => {
+    pantalla.innerText += 2;
+});
+
+botonTres.addEventListener("click", (e) => {
+    pantalla.innerText += 3;
+});
+
+botonCuatro.addEventListener("click", (e) => {
+    pantalla.innerText += 4;
+});
+
+botonCinco.addEventListener("click", (e) => {
+    pantalla.innerText += 5;
+});
+
+botonSeis.addEventListener("click", (e) => {
+    pantalla.innerText += 6;
+});
+
+botonSiete.addEventListener("click", (e) => {
+    pantalla.innerText += 7;
+});
+
+botonOcho.addEventListener("click", (e) => {
+    pantalla.innerText += 8;
+});
+
+botonNueve.addEventListener("click", (e) => {
+    pantalla.innerText += 9;
+});
