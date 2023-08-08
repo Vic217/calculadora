e4e483d7df088fda8607415f3c190d18564cccd9
VicGar21 <victorgarp15@hotmail.com>
2023-08-08 08:55:56 -0600

    Refactoriza las funciones para ingresar numeros en pantalla

--- app.js
+++ app.js
@@ -72,16 +72,7 @@ function operate(primerNum, operador, segundoNum){
 // Variables de control
 const pantalla = document.getElementById("operacion");
 let resultadoEnPantalla = document.getElementById("valor");
-const botonCero = document.getElementById("cero");
-const botonUno = document.getElementById("uno");
-const botonDos = document.getElementById("dos");
-const botonTres = document.getElementById("tres");
-const botonCuatro = document.getElementById("cuatro");
-const botonCinco = document.getElementById("cinco");
-const botonSeis = document.getElementById("seis");
-const botonSiete = document.getElementById("siete");
-const botonOcho = document.getElementById("ocho");
-const botonNueve = document.getElementById("nueve");
+let numeros = document.querySelectorAll(".btnNumerico");
 const botonPunto = document.getElementById("punto");
 let arrPantalla = [];
 let puntos = [];
@@ -102,45 +93,11 @@ const botonResultado = document.getElementById("resultado");
 const del = document.getElementById("borrar");
 const c = document.getElementById("limpiar");
 
-// Agregar el valor del botón en la pantalla
-botonCero.addEventListener("click", (e) => {
-    pantalla.innerText += 0;
-});
-
-botonUno.addEventListener("click", (e) => {
-    pantalla.innerText += 1;
-});
-
-botonDos.addEventListener("click", (e) => {
-    pantalla.innerText += 2;
-});
-
-botonTres.addEventListener("click", (e) => {
-    pantalla.innerText += 3;
-});
-
-botonCuatro.addEventListener("click", (e) => {
-    pantalla.innerText += 4;
-});
-
-botonCinco.addEventListener("click", (e) => {
-    pantalla.innerText += 5;
-});
-
-botonSeis.addEventListener("click", (e) => {
-    pantalla.innerText += 6;
-});
-
-botonSiete.addEventListener("click", (e) => {
-    pantalla.innerText += 7;
-});
-
-botonOcho.addEventListener("click", (e) => {
-    pantalla.innerText += 8;
-});
-
-botonNueve.addEventListener("click", (e) => {
-    pantalla.innerText += 9;
+// Agregar el valor de los botones en la pantalla
+numeros.forEach(numero =>{
+    numero.addEventListener("click", (e) => {
+        pantalla.innerText +=  e.target.innerText;
+    });
 });
 
 botonPunto.addEventListener("click", (e) => {
